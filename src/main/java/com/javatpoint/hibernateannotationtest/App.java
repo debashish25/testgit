751dab63b972b1c37e6be30a77259a2e0cdcc72b
debashish25 <34775131+debashish25@users.noreply.github.com>
2023-03-19 15:27:46 +0530

    commit1

--- src/main/java/com/javatpoint/hibernateannotationtest/App.java
+++ src/main/java/com/javatpoint/hibernateannotationtest/App.java
@@ -17,6 +17,7 @@ import org.hibernate.cfg.Configuration;
 public class App {
 	public static void main(String[] args) {
 		System.out.println("Hello java");
+		System.out.println("Hello java1");
 		System.out.println("Hello World new !");
 		Configuration cfg = new Configuration();
 		cfg.configure();
