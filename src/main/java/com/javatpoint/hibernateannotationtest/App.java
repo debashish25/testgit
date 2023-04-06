f32309df386927d5b57da5cd5edb5863d5363b0e
LENOVO <mukherjeedm25@gmail.com>
2023-04-06 16:37:48 +0530

    test

--- src/main/java/com/javatpoint/hibernateannotationtest/App.java
+++ src/main/java/com/javatpoint/hibernateannotationtest/App.java
@@ -17,7 +17,7 @@ import org.hibernate.cfg.Configuration;
 public class App {
 	public static void main(String[] args) {
 		System.out.println("Hello java");
-		System.out.println("Hello World newtest!");
+		System.out.println("Hello World newtestt!");
 		Configuration cfg = new Configuration();
 		cfg.configure();
 		SessionFactory sfg = cfg.buildSessionFactory();
