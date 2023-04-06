122b30039c5d8b89a03d05b5470ffa19b038edc1
debashish25 <34775131+debashish25@users.noreply.github.com>
2023-04-06 22:08:43 +0530

    Update App.java
    changes in main

--- src/main/java/com/javatpoint/hibernateannotationtest/App.java
+++ src/main/java/com/javatpoint/hibernateannotationtest/App.java
@@ -17,7 +17,7 @@ import org.hibernate.cfg.Configuration;
 public class App {
 	public static void main(String[] args) {
 		System.out.println("Hello java");
-		System.out.println("Hello World newtestt1234!");
+		System.out.println("Hello World newtestt12345!");
 		Configuration cfg = new Configuration();
 		cfg.configure();
 		SessionFactory sfg = cfg.buildSessionFactory();
