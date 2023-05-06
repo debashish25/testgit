c31072e2dc0151f026ff185317d2383925698b2a
LENOVO <mukherjeedm25@gmail.com>
2023-05-06 15:53:33 +0530

    Merge branch 'main' into test
    # Conflicts:
    #	.idea/workspace.xml
    #	src/main/java/com/javatpoint/hibernateannotationtest/App.java

--- src/main/java/com/javatpoint/hibernateannotationtest/App.java
+++ src/main/java/com/javatpoint/hibernateannotationtest/App.java
@@ -16,6 +16,7 @@ import org.hibernate.cfg.Configuration;
 // */
 public class App {
 	public static void main(String[] args) {
+		System.out.println("Hello java");
 		System.out.println("Hello javatestingnew");
 		Configuration cfg = new Configuration();
 		cfg.configure();
