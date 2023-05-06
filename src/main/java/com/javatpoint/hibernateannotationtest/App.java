5da5c3ff41f68e730b5e8b264847f9bf830a5e10
debashish25 <34775131+debashish25@users.noreply.github.com>
2023-05-06 15:32:03 +0530

    Update App.java
    test

--- src/main/java/com/javatpoint/hibernateannotationtest/App.java
+++ src/main/java/com/javatpoint/hibernateannotationtest/App.java
@@ -16,7 +16,7 @@ import org.hibernate.cfg.Configuration;
 // */
 public class App {
 	public static void main(String[] args) {
-
+		System.out.println("heelo");
 		Configuration cfg = new Configuration();
 		cfg.configure();
 		SessionFactory sfg = cfg.buildSessionFactory();
