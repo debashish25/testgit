8c03733e6eadefbcdbf9c36eb18db551a9210e99
debashish25 <34775131+debashish25@users.noreply.github.com>
2023-05-06 16:11:48 +0530

    Update App.java (#11)
    changes in test 2 new

--- src/main/java/com/javatpoint/hibernateannotationtest/App.java
+++ src/main/java/com/javatpoint/hibernateannotationtest/App.java
@@ -20,6 +20,7 @@ public class App {
 		System.out.println("new data in master");
 		System.out.println("heelo");
 		System.out.println("heelojava");
+		System.out.println("new data to test2 again);
 		Configuration cfg = new Configuration();
 		cfg.configure();
 		SessionFactory sfg = cfg.buildSessionFactory();
